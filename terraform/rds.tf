27f383c47a83b2c31b92a6d7eb1825eddac84e37
Alexis Colonna <alexis.colonna@yahoo.fr>
2025-11-12 10:41:40 +0100

    Change backup retention period for RDS cluster
    Reduce backup retention period from 15 to 1 day.

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -83,7 +83,7 @@ resource "aws_rds_cluster" "app5-rds-cluster" {
 resource "aws_rds_cluster" "app6-rds-cluster" {
   cluster_identifier      = "app6-rds-cluster"
   allocated_storage       = 10
-  backup_retention_period = 15
+  backup_retention_period = 1
   tags = {
     git_commit           = "079fe74f6b96d887c245664fbd8cf676c92f20e5"
     git_file             = "terraform/aws/rds.tf"
