70781488624f99a9833b17b8eda894c2465b9c5e
Alexis <alexis.colonna@symbioticsec.ai>
2026-06-04 10:51:53 +0200

    vuln created

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -100,7 +100,7 @@ resource "aws_rds_cluster" "app6-rds-cluster" {
 resource "aws_rds_cluster" "app7-rds-cluster" {
   cluster_identifier      = "app7-rds-cluster"
   allocated_storage       = 10
-  backup_retention_period = 25
+  backup_retention_period = 1
   tags = {
     git_commit           = "079fe74f6b96d887c245664fbd8cf676c92f20e5"
     git_file             = "terraform/aws/rds.tf"
